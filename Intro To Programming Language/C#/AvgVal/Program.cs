dba9526e95cb754ca0b6c1c2268d9bdfdcfb7cac
Rafael Rezyapov <rezograf@gmail.com>
2022-09-14 12:32:17 +0300

    add AvgVal доп задачка по поиску среднего

---
+++ Intro To Programming Language/C#/AvgVal/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти среднее по величине значение
+
+int[] arr = { 13, 28, 5, 23, 12, 33, 14 };
+Console.Clear();
+Console.WriteLine("Имеем массив:");
+foreach (int e in arr)
+    Console.Write($"{e} ");
+
+double avgVal = 0;
+foreach (int e in arr)
+    avgVal = avgVal + e;
+int len = arr.Length;
+avgVal = avgVal / len;
+//Console.Write($"\nСреднее значение: ");
+Console.WriteLine(avgVal);
+
+double nearVal = avgVal - arr[0];
+int posOfAvgVal = 0;
+for (int i = 1; i < len; i++)
+{
+    if (Math.Abs(avgVal - arr[i]) < nearVal)
+    {
+        posOfAvgVal = i;
+        nearVal = Math.Abs(avgVal - arr[i]);
+    }
+}
+Console.WriteLine($"\nСреднее значание в массиве -> {arr[posOfAvgVal]}");
