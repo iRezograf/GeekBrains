214e9c67fddc6c6c8b7215fafe1073420d5428f1
Rafael Rezyapov <rezograf@gmail.com>
2022-09-18 13:45:22 +0300

    add commnt in task43

--- Intro To Programming Language/C#/task43/Program.cs
+++ Intro To Programming Language/C#/task43/Program.cs
@@ -7,6 +7,16 @@
 
 double [] Point(double [][] arr)
 {
+    //Точка пересечения двух прямых:
+    // у =  arr[0][0]*x + arr[0][1]
+    // у =  arr[1][0]*x + arr[1][1]
+    // k1 = arr[0][0]
+    // b1 = arr[0][1]
+    // k2 = arr[1][0]
+    // b2 = arr[1][1]
+    // вычисляется по формуле:
+    // x = (b2-b1)/(k1-k2)
+    // y = k1*x + b1
     double [] res = new double [2];
     res[0] = (arr[1][1] - arr[0][1]) / (arr[0][0] - arr[1][0]);
     res[1] = arr[0][0] * res[0] + arr[0][1];
@@ -14,24 +24,24 @@ double [] Point(double [][] arr)
 }
 
 MyClasses mc = new MyClasses();
-double[][] arr = new double[2][];
+double[][] array = new double[2][];
 string prompt;
 
 prompt = "введите параметры 1-й линии (y = k * x + b) в формате k, b: ";
-arr[0] = mc.CreateArrFromKeyboard(prompt);
+array[0] = mc.CreateArrFromKeyboard(prompt);
 prompt = "введите параметры 2-й линии (y = k * x + b) в формате k, b: ";
-arr[1] = mc.CreateArrFromKeyboard(prompt);
+array[1] = mc.CreateArrFromKeyboard(prompt);
 
-if (arr[0][0] == arr[1][0])
+if (array[0][0] == array[1][0])
     Console.WriteLine("прямые не пересекаются ...");
 else
 {
-    double [] point = Point(arr);
+    double [] point = Point(array);
     
-    Console.Write($"b1 = {arr[0][1]}, ");
-    Console.Write($"k1 = {arr[0][0]}, ");
-    Console.Write($"b2 = {arr[1][1]}, ");
-    Console.Write($"k2 = {arr[1][0]} -> ");
+    Console.Write($"b1 = {array[0][1]}, ");
+    Console.Write($"k1 = {array[0][0]}, ");
+    Console.Write($"b2 = {array[1][1]}, ");
+    Console.Write($"k2 = {array[1][0]} -> ");
     Console.Write($"( x = {point[0]}, ");
     Console.WriteLine($" y = {point[1]} )");
 }
