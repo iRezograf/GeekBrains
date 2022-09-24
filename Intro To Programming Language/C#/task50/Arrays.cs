661f74f77b868f4001664da52ab4609f8e431cf9
Rafael Rezyapov <rezograf@gmail.com>
2022-09-24 17:33:43 +0300

    add task50

---
+++ Intro To Programming Language/C#/task50/Arrays.cs
@@ -0,0 +1,173 @@
+﻿public class Arrays
+{
+    public int[] CreateArray(int count, int minVal, int maxVal)
+    {
+        int[] result = new int[count];
+        Random ran = new Random();
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = ran.Next(minVal, maxVal + 1);
+        }
+        return result;
+    }
+
+    public int[][] CreateArray(int row, int column, int minVal, int maxVal)
+    {
+        int[][] result = new int[row][];
+        for (int i = 0; i < row; i++)
+        {
+            result[i] = CreateArray(column, minVal, maxVal + 1);
+        }
+        return result;
+    }
+
+    public void CreateArray(int row, int column, int minVal, int maxVal, out int[,] array)
+    {
+        array = new int[row, column];
+        int[] columns; // = new int[column];
+        for (int i = 0; i < row; i++)
+        {
+            columns = CreateArray(column, minVal, maxVal + 1);
+            for (int j = 0; j < column; j++)
+            {
+                array[i, j] = columns[j];
+            }
+        }
+    }
+
+    public void PrintArray(int[] arr)
+    {
+        Console.Write("[");
+        foreach (int a in arr)
+            Console.Write($"{a}, ");
+        Console.Write($"\b\b] ");
+    }
+
+    public void PrintArray(double[] arr)
+    {
+        Console.Write("[");
+        foreach (double a in arr)
+            Console.Write($"{a:F2}, ");
+        Console.Write($"\b\b] ");
+    }
+
+    public void PrintArray(int[][] array)
+    {
+        Console.WriteLine("[");
+        foreach (int[] row in array)
+        {
+            Console.Write("[");
+            foreach (int e in row)
+            {
+                Console.Write($"{e}, ");
+            }
+            Console.Write("\b\b]\n");
+        }
+        Console.WriteLine("]");
+    }
+
+    public void PrintArray(int[,] array)
+    {
+        Console.WriteLine("[");
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("[");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j],10:F2}, ");
+            }
+            Console.Write("\b\b]\n");
+        }
+        Console.WriteLine("]");
+    }
+
+    public int[] CreateArrFromKeyboard()
+    {
+        Console.Write($"введите  массив в формате x1, x2,... : ");
+        string str = Console.ReadLine();
+        string[] strArray = str.Split(',');
+        int len = strArray.Length;
+        int[] arr = new int[len];
+
+        if (len != 0)
+        {
+            int i = 0;
+            foreach (string item in strArray)
+            {
+                bool isInt = Int32.TryParse(item, out int res);
+                if (isInt)
+                {
+                    arr[i] = res;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("введено не число ...");
+                    int[] notInt = new int[0];
+                    return notInt;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public int[] CreateArrFromKeyboard(string prompt)
+    {
+        Console.Write(prompt);
+        string str = Console.ReadLine();
+        string[] strArray = str.Split(',');
+        int len = strArray.Length;
+        int[] arr = new int[len];
+
+        if (len != 0)
+        {
+            int i = 0;
+            foreach (string item in strArray)
+            {
+                bool isInt = Int32.TryParse(item, out int res);
+                if (isInt)
+                {
+                    arr[i] = res;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("введено не число ...");
+                    int[] notInt = new int[0];
+                    return notInt;
+                }
+            }
+        }
+        return arr;
+    }
+    public double[] AverageByColumns(int[][] array)
+    {
+        double[] result = new double[array[0].Length];
+        for (int j = 0; j < array[0].Length; j++)
+        {
+            double summa = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                summa = summa + array[i][j];
+            }
+            result[j] = summa / array.Length;
+        }
+        return result;
+    }
+
+    public double[] AverageByColumns(int[,] array)
+    {
+        double[] result = new double[array.GetLength(1)];
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double summa = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                summa = summa + array[i, j];
+            }
+            result[j] = summa / array.GetLength(1);
+        }
+        return result;
+    }
+
+}
---
+++ Intro To Programming Language/C#/task50/task50.cs
@@ -0,0 +1,26 @@
+﻿    Console.WriteLine(
+    "// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,\n" +
+    "// и возвращает значение этого элемента или же указание, что такого элемента нет.\n" +
+    //"//Например, задан массив:\n" +
+    //"//1 4 7 2\n" +
+    //"//5 9 2 3\n" +
+    //"//8 4 2 4\n" +
+    //"//17 -> такого числа в массиве нет"
+    "");
+
+    Arrays a = new Arrays();
+
+    int row = new Random().Next(1, 11);
+    int column = new Random().Next(1, 11);
+    int minValue = -10;
+    int maxValue = 10;
+    Console.WriteLine($"row = {row}, column = {column}");
+    a.CreateArray(row, column, minValue, maxValue, out int[,] array);
+    a.PrintArray(array);
+    int[] position = a.CreateArrFromKeyboard("введите позиции элемента в формате i,j ");
+
+    if (position[0] < row & position[1] < column)
+        Console.WriteLine($"элемент с индексом [{position[0]},{position[1]}] = {array[position[0], position[1]]}");
+    else
+        Console.WriteLine($"элемента с индексом [{position[0]},{position[1]}] в массиве нет");
+
