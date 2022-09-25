a2532c79c957f1eced784d14ea74cf8c990d71c3
Rafael Rezyapov <rezograf@gmail.com>
2022-09-25 22:17:54 +0300

    refactoring katalog & add task56

---
+++ Intro To Programming Language/C#/task56/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+Arrays arrays = new Arrays();
+int row = 3;
+int column = 5;
+int minVal = -10;
+int maxVal = 10;
+
+int[][] array = arrays.CreateArray(row, column, minVal, maxVal);
+Console.WriteLine("Задан массив:");
+arrays.PrintArray(array);
+
+int sum = 0;
+int minSum = sum;
+for (int j = 0; j < array[0].Length; j++)
+{
+    sum += array[0][j];
+}
+minSum = sum;
+int rowNumberWithMinSum = 0;
+for (int i = 1; i < array.Length; i++)  // для массива массивов array[][] 
+                                        // количество строк = array.Length
+{
+    sum = 0;
+    for (int j = 0; j < array[i].Length; j++)
+    {
+        sum += array[i][j];
+    }
+    if (sum < minSum)
+    {
+        minSum = sum;
+        rowNumberWithMinSum = i;
+    }
+}
+Console.WriteLine($"Строка с наименьшей суммой {minSum} элементов:\n");
+arrays.PrintArray(array[rowNumberWithMinSum]);
