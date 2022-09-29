1268d3b88b69b8c473e8f4cb3f5eb303cc04823e
Rafael Rezyapov <rezograf@gmail.com>
2022-09-29 20:13:33 +0300

    add library MyStudyLib class array (общ. файла и обращение к нему с разной задачи.) & task 68

--- Intro To Programming Language/C#/MyStudyLib/Arrays.cs
+++ Intro To Programming Language/C#/MyStudyLib/Arrays.cs
@@ -247,4 +247,23 @@ public class Arrays
         if (m == n) return n;
         else return m + SumNElements(m + 1, n);
     }
+
+
+    public int Akkerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if ((m > 0) && (n == 0))
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else if ((m > 0) && (n > 0))
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+        else
+            return n + 1;
+    }
 }
---
+++ Intro To Programming Language/C#/task68/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyStudyLib;
+// Задача 68: Напишите программу вычисления функции 
+// Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Arrays ar = new Arrays();
+Console.Clear();
+int[] array = ar.CreateArrFromKeyboard("Задайте числа для " +
+                                    "для вычисления функции Аккермана в формате  m,n: ");
+Console.WriteLine($"m = {array[0]}; n = {array[1]} -> A(m,n) = {ar.Akkerman(array[0], array[1])}");
