2005625f5b0bc3d6f378f3f381fa9c099abf2def
Rafael Rezyapov <rezograf@gmail.com>
2022-09-29 12:13:55 +0300

    add task60

---
+++ Intro To Programming Language/C#/task60/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+Arrays arrays = new Arrays();
+int len = 2;
+//int [] lineArray = new int [len*len*len];
+int [] lineArray = arrays.CreateArrayUniqValue(8, 10, 99);
+
+int [,,] array = new int [len,len,len];
+
+int pos = 0;
+for (int i = 0; i < len; i++)
+{
+    for (int j = 0; j < len; j++)
+    {
+        for (int m = 0; m < len; m++)
+        {
+            array[m,j,i] = lineArray[pos++];
+            Console.Write($"{array[m,j,i]} ({m}{j}{i}) ");
+        }
+        Console.Write($"\n");
+        
+    }
+}
---
+++ Intro To Programming Language/C#/task62/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Arrays arrays = new Arrays();
+
+Console.WriteLine(4534%10);
+
+
+int[] dimentions = arrays.CreateArrFromKeyboard("введите размер матрицы А в формате: i,j: ");
+int[,] array = new int[dimentions[0], dimentions[1]];
+
+for (int i = 0; i < dimentions[0]; i++)
+{
+    for (int j = 0; j < dimentions[1]; j++)
+    {
+        array[i, j] = i * dimentions[1] + j;
+        array[i, j] = 0;
+        //Console.WriteLine(array[i, j].ToString("00.00"));
+    }
+}
+
+array[0,0] = 0;
+Right(array, 0, 0, dimentions[1]-1);
+arrays.PrintArray(array);
+
+
+void Right(int [,] array, int positionRow, int positionColumn, int countHope)
+{
+    for (int i = 1; i <= countHope; i++)
+    {
+        array[positionRow,positionColumn+i] = array[positionRow,positionColumn]+1;
+        Console.Write(array[positionRow,positionColumn+i]+ "");
+        //positionRow++;
+        //positionColumn++;
+    }
+}
+/* void UpRight(int [,] array, int i, int j, int count)
+{
+    
+}
+
+void DownLeft(int [,] array, int positionRow, int positionColumn, int countHope, int value)
+{
+    for (int i = 1; i <= countHope; i++)
+    {
+        array[positionRow,positionColumn+i] = array[positionRow,positionColumn]+1;
+    }
+} */
