786e442337844096e9ac25bb7d4a5295fbc50156
Rafael Rezyapov <Rezograf@gmail.com>
2022-11-07 17:38:35 +0300

    add HW5/t2.py

--- Intro To Programming Language/Python/HW5/t2.py
+++ Intro To Programming Language/Python/HW5/t2.py
@@ -5,50 +5,27 @@ from typing import List, Any
 
 
 def RLE(data):  # run-length encoding
-    # если серия закнчивается в одном буфере, а продолжается в следующем
-    # то мой RLE распишет его как два вхождения.
-    # это, не нарушает принципа RLE, а процент сжатия становится меньше на велиичину
-    # (8/BUFF_SIZE), что пренебрежительно мало для BUFF_SIZE = 4096
-    l = []
+    result = ''
     if not data:
-        return l
+        return result
     char = data[0]
     cnt = 0
     for item in data:
         if char == item:
             cnt = cnt + 1
         else:
-            l.append(str(cnt) + str(char))
+            result += str(cnt) + str(char)
             cnt = 1
             char = item
-    l.append(str(cnt) + str(char))
-    return l
-
-
-# end def
-
-
-def convert_txt_to_rle(txt_file_name, rle_file_name):
-    f_txt = open(txt_file_name, 'r')
-    f_rle = open(rle_file_name, 'w', BUFF_SIZE)
-    d_txt = f_txt.read(BUFF_SIZE)
-    while d_txt:
-        d_rle = RLE(d_txt)
-        for item in d_rle:
-            f_rle.write(str(item))
-        d_txt = f_txt.read(BUFF_SIZE)
-    f_rle.close
-    f_txt.close
-
+    result += str(cnt) + str(char)
+    return result
 
 def RLDe(data):  # run-length Decoding
-    # data = '33a6s3d1e3q44q3x2y1z3a6s3d1e3q4q3x2y1z'
-    # print(data)
-    # print ('--------')
     lst = []
     if not data:
         return lst
     i = 0
+    decoded_str = ''
     while i < len(data) - 1:
         d = data[i]
         digit = ''
@@ -56,66 +33,53 @@ def RLDe(data):  # run-length Decoding
             digit += d
             i = i + 1
             d = data[i]
-        # print(digit, end='')
-        decoded_str = ''
         for j in range(int(digit)):
             decoded_str += data[i]
-        # print(decoded_str)
         i = i + 1
-    # for item in lst:
-    # print(item)
     return decoded_str
-
-
-
-
-def convert_rle_to_txt(rle_file_name, txt_file_name):
-    f_rle = open(rle_file_name, 'r', BUFF_SIZE)
-    f_txt = open(txt_file_name, 'w')
-    d_rle = f_rle.read(BUFF_SIZE)
-    #cut_BUFF_SIZE
-    # ошибка на границе буфера
-    while d_rle:
-        d_txt = RLDe(d_rle)
-        for item in d_txt:
-            f_txt.write(str(item))
-        d_rle = f_rle.read(BUFF_SIZE)
+def convert_txt_to_rle(txt_file_name, rle_file_name):
+    f_txt = open(txt_file_name, 'r')
+    f_rle = open(rle_file_name, 'w')
+    d_txt = f_txt.read()
+    d_rle = RLE(d_txt)
+    f_rle.write(d_rle)
     f_rle.close
     f_txt.close
 
-
-# end def
-
-def print_file(file_name):
-    f_txt = open(file_name, 'r', BUFF_SIZE)
-    txt = f_txt.read(BUFF_SIZE)
-    while txt:
-        print(txt, end='')
-        txt = f_txt.read(BUFF_SIZE)
+def convert_rle_to_txt(rle_file_name, txt_file_name):
+    f_rle = open(rle_file_name, 'r')
+    f_txt = open(txt_file_name, 'w')
+    d_rle = f_rle.read()
+    f_txt.write(RLDe(d_rle))
+    f_rle.close
     f_txt.close
 
-
-# end def
-
+def print_file(file_name):
+    f_txt = open(file_name, 'r')
+    txt = f_txt.read()
+    while txt:
+        print(txt)
+        txt = f_txt.read()
+    f_txt.close
 
 # data = 'aaassssssddddddeqqqqqqqxxxyyz'
-# f_txt = open('t2.txt', 'w', BUFF_SIZE)
+# f_txt = open('t2.txt', 'w')
 # f_txt.write(data)
 
-BUFF_SIZE = 16  # взял маленькое значение для наглядности
 print('\n' * 10)
 
-# convert_txt_to_rle('t2.txt','t2.rle')
-# print_file('t2.txt')
-# print()
-# print_file('t2.rle')
+print('конвертация текста в RLE')
+print_file('t2.txt')
+convert_txt_to_rle('t2.txt','t2.rle')
+print_file('t2.rle')
+print()
 
 # print()
-# data = '33a6s3d1e3q44q3x2y1z3a6s3d1e3q4q3x2y1z'
+# data = '33a6s3d1e3q44q11x2y11z'
 # RLDe(data)
 # print('\n' * 2)
 
-convert_rle_to_txt('t2.rle', 't2.txt')
+print('конвертация RLE в текст')
 print_file('t2.rle')
-print()
+convert_rle_to_txt('t2.rle', 't2.txt')
 print_file('t2.txt')
