8911ac4171c9ee33401d38470aa024609f890645
Rafael Rezyapov <rezograf@gmail.com>
2022-11-10 16:52:29 +0300

    add t4.py

--- Intro To Programming Language/Python/HW5/t4.py
+++ Intro To Programming Language/Python/HW5/t4.py
@@ -6,8 +6,14 @@ import re
 
 def get_structure(multinom):
     # multinom - член у многочлена
+    if not len(multinom):
+        return {}
+    znak = 1
     ratio = 0  # коэффициент
     power = 0  # степень
+    if multinom[0] == '-':
+        znak = -1
+        multinom = multinom[1:]
     if multinom.find('^') == -1 and multinom.find('*') == -1:
         ratio = int(multinom)
         power = 0
@@ -18,7 +24,9 @@ def get_structure(multinom):
         power = int(multinom[multinom.find('^') + 1:])
         if multinom.find('*') != -1:
             ratio = int(multinom[:multinom.find('*')])
-    return {power: ratio}
+        else:
+            ratio = 1
+    return {power: znak * ratio}
 
 
 def f_sort(val):
@@ -29,62 +37,68 @@ def f_sort(val):
     return ratio
 
 
-m1 = '5 * x ^ 22 + x^4 + 2*x^2 + 5*x + 6'
-m2 = '7*x^22 + 5*x^10+ 3*x^3 +  8*x + 3'
+def deparsing(item):
+    # power: ratio
+    m = item.split(':')
+    power = m[0]
+    ratio = m[1]
+    #print(f'{power}:{ratio}')
+    if ratio > '0':
+        ratio = "+"+ratio
+    if ratio == '0':
+        return ' '
+    if power == '0':
+        return f'{ratio}'
+    if power == '1':
+        return f'{ratio}*x'
+    if ratio == '+1' and power > '1':
+        return f'x^{power}'
+    if ratio == '-1' and power > '1':
+        return f'-x^{power}'
+    else:
+        return f'{ratio}*x^{power}'
 
-list_m1 = m1.replace(' ', '').split('+')
-list_m2 = m2.replace(' ', '').split('+')
-list_m1 = m1.replace(' ', '').split('+')
-list_m2 = m2.replace(' ', '').split('+')
-# print(list_m1)
-# print(list_m2)
+
+str1 = '5 * x ^ 22 - x^4 - 2*x^2 + 5*x - 6'
+str2 = '-3*x^22 + 5*x^10 -3*x^3 -  8*x + 3'
+
+list_m1 = str1.replace(' ', '')
+list_m2 = str2.replace(' ', '')
+
+list_m1 = list_m1.replace('+', '.+')
+list_m1 = list_m1.replace('-', '.-')
+
+list_m2 = list_m2.replace('+', '.+')
+list_m2 = list_m2.replace('-', '.-')
+
+list_m1 = list_m1.split('.')
+list_m2 = list_m2.split('.')
 
 m1_structure = {}
 m2_structure = {}
 for item in list_m1:
     m1_structure.update(get_structure(item))
-
 for item in list_m2:
     m2_structure.update(get_structure(item))
-# print(m1_structure)
-# print(m2_structure)
 
-list = []
+m_structure = []
 for m1 in m1_structure:
     val = 0
-
     if m1 in m2_structure:
         val = m2_structure.pop(m1)
-        # print(f'{m1}:{m1_structure[m1]}:{val}')
-    list.append(f'{m1}:{m1_structure[m1]}:{val}')
+    m_structure.append(f'{m1}:{int(m1_structure[m1]) + int(val)}')  # сложение коэффициентов
 for m2 in m2_structure:
-    list.append(f'{m2}:{m2_structure[m2]}')
+    m_structure.append(f'{m2}:{m2_structure[m2]}')
 
-list.sort(reverse=True, key=f_sort)
-for i in list:
-    print(i)
-print('depars')
-for item in list:
-    m = item.split(':')
-    ratio = 0
-    #print(m)
-    for i in range(1,len(m)):
-        #print(m[i])
-        ratio += int(m[i])
-        if ratio == 0:
-            print('')
-        if ratio == 1:
-            print(f'x^{m[0]}')
-        else:
-            print(f'{ratio}*x^{m[0]}')
-    print('--------')
+m_structure.sort(reverse=True, key=f_sort)
 
+sum_m1_m2 = ''
+for i in m_structure:
+    sum_m1_m2 = sum_m1_m2 + deparsing(i)
+if sum_m1_m2[0] == "+":
+    sum_m1_m2=sum_m1_m2[1:]
 
-# print(m1_structure)
-# print(m2_structure)
-
-# thisdict.update({"color": "red"})
-
-
-# items()	Returns a list containing a tuple for each key value pair
-# keys()	Returns a list containing the dictionary's keys
+print(str1)
+print(str2)
+print('сумма многочленов:')
+print(sum_m1_m2)
