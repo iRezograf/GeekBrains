cf484f496fea19a7f9cee8e02419e692f41c8dfe
Rafael Rezyapov <rezograf@gmail.com>
2022-11-12 00:14:47 +0300

    add calc.py with input

--- Intro To Programming Language/Python/HW6/calc.py
+++ Intro To Programming Language/Python/HW6/calc.py
@@ -8,7 +8,7 @@ def expression_in_brackets(expression):
     x = expression
     while m != []:
         for i in m:
-            print(f'i: {i}->',end='')
+            # print(f'i: {i}->',end='')
             x = calc_in_brackets(x, i)
         m = pattern.findall(x)
     return x
@@ -20,7 +20,7 @@ def calc_in_brackets(full_expression, expression):
     sum = 0
     for i in m:
         sum += int(i)
-    print(sum)
+    # print(sum)
     full_expression = str(full_expression).replace(expression, str(sum))
     return full_expression
 
@@ -30,12 +30,12 @@ def multiplicate(full_expression):
     while m != []:
         for i in m:
             pos = str(i).find('*')
-            print("pos:"+str(i)[:pos])
-            print("pos:"+str(i)[pos+1:])
+            # print("pos:"+str(i)[:pos])
+            # print("pos:"+str(i)[pos+1:])
             value = float(str(i)[:pos])*float(str(i)[pos+1:])
         full_expression = str(full_expression).replace(i, str(value))
         m = pattern.findall(full_expression)
-    #print(full_expression)
+    # print(full_expression)
     return full_expression
 
 def devide(full_expression):
@@ -50,7 +50,7 @@ def devide(full_expression):
                 print('деление на ноль недопустимо!')
         full_expression = str(full_expression).replace(i, str(value))
         m = pattern.findall(full_expression)
-    print(full_expression)
+    # print(full_expression)
     return full_expression
 
 def plus_minus(full_expression):
@@ -63,24 +63,25 @@ def plus_minus(full_expression):
     return full_expression
 
 calc_str = '((1+2)+3)*2*2/(3+9)+(12+13)'
+calc_str = input('введите строку для расчета:')
 calc_str = calc_str.replace(" ", '')
-print(f'\n\nисходное выражение: {calc_str}')
+#print(f'\n\nисходное выражение: {calc_str}')
 
 calc_str = multiplicate(calc_str)
-print(f'multiplicate: {calc_str}')
+# print(f'multiplicate: {calc_str}')
 calc_str = devide(calc_str)
-print(f'devide: {calc_str}')
+# print(f'devide: {calc_str}')
 calc_str = expression_in_brackets(calc_str)
-print(f'expression_in_brackets: {calc_str}')
+# print(f'expression_in_brackets: {calc_str}')
 
 calc_str = multiplicate(calc_str)
-print(f'multiplicate: {calc_str}')
+# print(f'multiplicate: {calc_str}')
 
 calc_str = devide(calc_str)
-print(f'devide: {calc_str}')
+# print(f'devide: {calc_str}')
 
 calc_str = plus_minus(calc_str)
-print(f'plus_minus: {calc_str}')
+# print(f'plus_minus: {calc_str}')
 
 print(float(calc_str))
 
