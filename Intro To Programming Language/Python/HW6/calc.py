947422f860fe17c565a7c9b31c9ed5226c1c2cb6
Rafael Rezyapov <rezograf@gmail.com>
2022-11-17 13:02:14 +0300

    rewrited calculate(full_expression): problem with addition

--- Intro To Programming Language/Python/HW6/calc.py
+++ Intro To Programming Language/Python/HW6/calc.py
@@ -89,7 +89,7 @@ def percentage(full_expression):
         ret = 'NaN'
 
 
-def calc_expression(expression) -> str:
+def calc_expression_(expression) -> str:
     expression = expression.replace(" ", '')
     expression = expression.replace(",", '.')
     expression = multiplicate(expression)
@@ -99,7 +99,6 @@ def calc_expression(expression) -> str:
     expression = divide(expression)
     expression = plus_minus(expression)
     return expression
-
 
 def prepare_expression(expression):
     full_expression = expression.replace(" ", '')
@@ -113,7 +112,7 @@ def prepare_expression(expression):
     return full_expression
 
 
-def brackets(full_expression):
+def calc_expression(full_expression):
     pattern = re.compile(r"\([-+ *\/.\d\s]+\)")
     m = pattern.search(full_expression)
     if not m:
@@ -121,63 +120,100 @@ def brackets(full_expression):
         return full_expression
 
     while m:
-        s = calculate(full_expression[m.start():m.end()])
-        full_expression = full_expression[:m.start()] + s + full_expression[m.end():]
+        print(full_expression[m.start():m.end()])
+        value = calculate(full_expression[m.start():m.end()])
+        #if float(value) > 0:
+        full_expression = full_expression[:m.start()] + "+" + value + full_expression[m.end():]
+        #else:
+        #    full_expression = full_expression[:m.start()] + value + full_expression[m.end():]
         # cprint(full_expression)
         m = pattern.search(full_expression)
     return full_expression
 
 
+def check_val_and_make_exp(full_expression, m, value):
+    if float(value) > 0:
+        full_expression = full_expression[:m.start()] + "+" + str(value) + full_expression[m.end():]
+    else:
+        full_expression = full_expression[:m.start()] + str(value) + full_expression[m.end():]
+    # print(f' check_val_and_make_exp Return Value:\t\t {full_expression}')
+    return full_expression
+
+
 def calculate(full_expression):
+    full_expression = full_expression.replace(")", '')
+    full_expression = full_expression.replace("(", '')
+
     m = re.search(r'[-+ *\ /][*\ /]', full_expression)
     if m:
         print('Недопустимая компбинация операций')
         return 'NaN'
-    full_expression = full_expression.replace(")", '')
-    full_expression = full_expression.replace("(", '')
 
     # pattern = 'r[-+]?[0-9.]+[*\/][-+]?[0-9.]+'
     m = re.search(r"[-+]?[0-9.]+[*\/][-+]?[0-9.]+", full_expression)
-    # деление
+    # dividing
     while m:
         mm = re.search(r'/', m[0])
-        print(f'mm= {mm}')
+        # print(f'/m:{m}')
+        # print(f'/mm= {mm}')
         if mm:
             d = full_expression[m.start():m.end()]
+            # print(d)
             if (d[mm.end():]) != '0':
                 value = float(d[:mm.start()]) / float(d[mm.end():])
-                full_expression = full_expression[:m.start()] + "+" + str(value) + full_expression[m.end():]
-                print(full_expression)
+                full_expression = check_val_and_make_exp(full_expression, m, value)
             else:
                 print('ошибка = деление на ноль')
                 return 'NaN'
 
         m = re.search(r"[-+]?[0-9.]+[*\/][-+]?[0-9.]+", full_expression)
-        print(m)
-        # умножение
+        # multiplication
         if m:
             mm = re.search(r'\*', m[0])
-        if mm:
-            d = full_expression[m.start():m.end()]
-            print(d)
-            value = float(d[:mm.start()]) * float(d[mm.end():])
-            full_expression = full_expression[:m.start()] + "+" + str(value) + full_expression[m.end():]
-        m = re.search(r"[-+]?[0-9.]+[*\/][-+]?[0-9.]+", full_expression)
-        if not m:
-            break
-    return full_expression
+            # print(f'*m:{m}')
+            # print(f'*mm= {mm}')
+            if mm:
+                d = full_expression[m.start():m.end()]
+                # print(d)
+                value = float(d[:mm.start()]) * float(d[mm.end():])
+                full_expression = check_val_and_make_exp(full_expression, m, value)
+    # addition
+    m = re.findall(r"[-+]?[0-9.]+", full_expression)
+    if m:
+        s = m.copy()
+    else:
+        return full_expression
+    # addition
+    value = 0
+    for i in s:
+        value += float(i)
+    return str(value)
 
+def summing(full_expression):
+    m = re.findall(r"[-+]?[0-9.]+", full_expression)
+    if m:
+        s = m.copy()
+    else:
+        return full_expression
+    # addition
+    value = 0
+    for i in s:
+        value += float(i)
+    return str(value)
 
 expression = '(49+ (200 *- 25/(5+7)*12))'
 # expression = '(49+ (200 *- 25*4,5/5)'
-expression = '1*5-122*23+4.0/-2'
-# print(re.search(r"[-+]?[0-9.]+[*\/][-+]?[0-9.]+", expression))
-# exit()
+expression = '2*(1+4)'
 print(f'expression : {expression}')
 expression = prepare_expression(expression)
-print(brackets(expression))
+expression = calc_expression(expression)
+print(expression)
+#print(brackets(expression))
+expression = summing(expression)
+print(expression)
+# brackets(expression)
 exit()
 
 # print('percentage:'+ percentage('150+0.5%'))
 # demo
-print(calc_expression(' 5--5'))
+print(calc_expression_(' 5--5'))
